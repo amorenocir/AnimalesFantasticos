d9ae05f7cb56550cd63ba1f9fc87bccfcab2b021
Andrea <amorenocir@uoc.edu>
2025-11-24 21:46:54 +0100

    Limpiza scripts movimiento

--- Assets/Scripts/Personaje.cs
+++ Assets/Scripts/Personaje.cs
@@ -2,24 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Personaje : MonoBehaviour
 {
-    [SerializeField] private float velocidad;
+    [SerializeField] private float velocidad = 5f; // Ajusta la velocidad desde el Inspector
 
     private Rigidbody2D rig;
+    private Vector2 movimiento;
 
     private void Awake()
     {
         rig = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
+        // Leer input cada frame
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Debug.Log("Horizontal: " + horizontal + " Vertical: " + vertical);
+        // Guardar vector de movimiento
+        movimiento = new Vector2(horizontal, vertical) * velocidad;
 
-        rig.linearVelocity = new Vector2(horizontal, vertical) * velocidad;
+    }
+
+    private void FixedUpdate()
+    {
+        // Aplicar velocidad al Rigidbody2D
+        rig.linearVelocity = movimiento;
     }
 }
+
